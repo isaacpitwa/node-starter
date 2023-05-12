10753007aa7d4d92f9bbd4c57cfdf6b78f6582c3
Isaac Pitwa <isaacpitwa256@gmail.com>
2023-05-12 21:49:43 +0300

    Initial Commit

---
+++ src/config/database.ts
@@ -0,0 +1,61 @@
+import * as mongoose from "mongoose";
+import { logger } from "./winston";
+
+export class Database {
+    public start = () => {
+        let dbName;
+
+        switch (process.env.NODE_ENV) {
+            case "test":
+                dbName = "node_starter_test";
+                break;
+            case "production":
+                dbName = "node_starter";
+                break;
+            default:
+                dbName = "node_starter_dev";
+                mongoose.set("debug", (coll: any, method: any, query: any, doc: any, opts: any) => {
+                    logger.info({
+                        dbQuery: {
+                            coll,
+                            method,
+                            query,
+                            doc,
+                            options: opts
+                        }
+                    });
+                });
+        }
+
+        const dbAddress = process.env.DB_HOST || "127.0.0.1";
+        const dbPort = process.env.DB_PORT || 27017;
+
+        const options = {
+            pass: "",
+            user: "",
+            useNewUrlParser: true
+        };
+
+        if (process.env.DB_AUTH === "true") {
+            options.user = process.env.DB_USER as string;
+            options.pass = process.env.DB_PASS as string;
+        }
+
+        mongoose.set("useCreateIndex", true);
+        mongoose
+            .connect(`mongodb://${dbAddress}:${dbPort}/${dbName}`, options)
+            .then(() => {
+                logger.log("info", `Connected to the database`);
+            })
+            .catch(err => {
+            if (err.message.indexOf("ECONNREFUSED") !== -1) {
+                logger.log("Error", `The server was not able to reach MongoDB. Maybe it's not running? :( ", ${err}`);
+                process.exit(1);
+            } else {
+                throw err;
+            }
+        });
+    }
+}
+
+export default new Database();
